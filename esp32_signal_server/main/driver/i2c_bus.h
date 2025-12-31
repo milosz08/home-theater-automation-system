8d0665aa0f8d2193ffe7c0303fd00a459fc6ca2f
Miłosz Gilga <gilgamilosz451@gmail.com>
2025-12-31 22:03:32 +0100

    refactor: update GPIO definitions to use GPIO_NUM macros for consistency

--- esp32_signal_server/main/driver/i2c_bus.h
+++ esp32_signal_server/main/driver/i2c_bus.h
@@ -2,10 +2,11 @@
 
 #include "esp_err.h"
 #include "driver/i2c.h"
+#include "driver/gpio.h"
 
 #define I2C_MASTER_NUM              I2C_NUM_0
-#define I2C_MASTER_SCL_IO           33
-#define I2C_MASTER_SDA_IO           27
+#define I2C_MASTER_SCL_IO           GPIO_NUM_33
+#define I2C_MASTER_SDA_IO           GPIO_NUM_27
 #define I2C_MASTER_FREQ_HZ          40000
 #define I2C_MASTER_TX_BUF_DISABLE   0
 #define I2C_MASTER_RX_BUF_DISABLE   0
--- esp32_signal_server/main/system/reset_button.h
+++ esp32_signal_server/main/system/reset_button.h
@@ -1,6 +1,8 @@
 #pragma once
 
-#define RESET_BUTTON_GPIO  34
+#include "driver/gpio.h"
+
+#define RESET_BUTTON_GPIO  GPIO_NUM_34
 #define HOLD_TIME_MS       3000
 
 void init_reset_button(void);
