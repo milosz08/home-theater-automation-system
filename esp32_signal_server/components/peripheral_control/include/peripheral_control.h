560f9ed87a5dc38eafa932ba4a3129e05728d878
Miłosz Gilga <gilgamilosz451@gmail.com>
2026-01-01 19:56:14 +0100

    create basic functions for peripheral projection screen component

--- esp32_signal_server/components/peripheral_control/include/peripheral_control.h
+++ esp32_signal_server/components/peripheral_control/include/peripheral_control.h
@@ -1,6 +1,8 @@
 #ifndef PERIPHERAL_CONTROL_H_
 #define PERIPHERAL_CONTROL_H_
 
-// TODO
+int peripheral_control_screen_down(void);
+int peripheral_control_screen_stop(void);
+int peripheral_control_screen_up(void);
 
 #endif
