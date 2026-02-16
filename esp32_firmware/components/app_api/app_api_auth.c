22b6ad7ec741a54a4b4d19d46cd5aba6c6486d33
Miłosz Gilga <gilgamilosz451@gmail.com>
2026-02-16 19:35:00 +0100

    refactor password change API to improve error handling and response consistency

--- esp32_firmware/components/app_api/app_api_auth.c
+++ esp32_firmware/components/app_api/app_api_auth.c
@@ -1,4 +1,5 @@
 #include "app_api_handlers.h"
+#include "app_api.h"
 #include "helper.h"
 #include "https_server.h"
 #include "nvs_manager.h"
@@ -21,16 +22,13 @@ esp_err_t app_api_auth_change_password(httpd_req_t *req)
   char buf[256];
   int ret, remaining = req->content_len;
 
-  if (remaining >= sizeof(buf))
-  {
-    httpd_resp_send_err(req, HTTPD_400_BAD_REQUEST, NULL);
-    return ESP_FAIL;
-  }
+  if (remaining >= sizeof(buf)) return API_RETURN_ERR(req, 400, ESP_ERR_NO_MEM);
+
   ret = httpd_req_recv(req, buf, remaining);
   if (ret <= 0)
   {
-    if (ret == HTTPD_SOCK_ERR_TIMEOUT) httpd_resp_send_408(req);
-    return ESP_FAIL;
+    if (ret == HTTPD_SOCK_ERR_TIMEOUT) return API_RETURN_ERR(req, 408, ESP_ERR_TIMEOUT);
+    return API_RETURN_ERR(req, 500, ESP_FAIL);
   }
 
   buf[ret] = '\0';
@@ -41,35 +39,22 @@ esp_err_t app_api_auth_change_password(httpd_req_t *req)
   if (httpd_query_key_value(buf, "old", old_pass, sizeof(old_pass)) != ESP_OK ||
       httpd_query_key_value(buf, "new", new_pass, sizeof(new_pass)) != ESP_OK)
   {
-    httpd_resp_send_err(req, HTTPD_400_BAD_REQUEST, NULL);
-    return ESP_FAIL;
+    return API_RETURN_ERR(req, 400, ESP_ERR_INVALID_ARG);
   }
 
   esp_err_t err = nvs_manager_load_str(AUTH_NVS_NS, AUTH_NVS_KEY, buf, sizeof(buf));
-  if (err != ESP_OK)
-  {
-    httpd_resp_send_500(req);
-    return ESP_FAIL;
-  }
+  if (err != ESP_OK) return API_RETURN_ERR(req, 500, err);
 
   size_t len_in = strlen(old_pass);
   size_t len_st = strlen(buf);
 
   if (len_in != len_st || helper_secure_memcmp(old_pass, buf, len_in) != 0)
   {
-    ESP_LOGW(TAG, "Password change failed: wrong old password");
-    httpd_resp_send_err(req, HTTPD_403_FORBIDDEN, NULL);
-    return ESP_FAIL;
+    return API_RETURN_ERR(req, 403, APP_ERR_INVALID_PASSWORD);
   }
   err = nvs_manager_save_str(AUTH_NVS_NS, AUTH_NVS_KEY, new_pass);
+  if (err != ESP_OK) return API_RETURN_ERR(req, 500, err);
 
-  if (err == ESP_OK)
-  {
-    ESP_LOGI(TAG, "password changed");
-    httpd_resp_set_status(req, "204 No Content");
-    return httpd_resp_send(req, NULL, 0);
-  }
-  ESP_LOGE(TAG, "NVS write error: %s", esp_err_to_name(err));
-  httpd_resp_send_500(req);
-  return ESP_FAIL;
+  ESP_LOGI(TAG, "password changed");
+  return api_return_ok(req);
 }
