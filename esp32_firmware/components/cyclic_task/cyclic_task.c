4c7c17305fc45f71178473437f2ea099251364e9
Miłosz Gilga <gilgamilosz451@gmail.com>
2026-02-18 02:00:05 +0100

    change min_free_heap into uptime_sec

--- esp32_firmware/components/cyclic_task/cyclic_task.c
+++ esp32_firmware/components/cyclic_task/cyclic_task.c
@@ -19,14 +19,14 @@ void env_monitor_task_entry(void *param)
     if (env_sensors_read_all(&env) == ESP_OK)
     {
       uint32_t free_heap = esp_get_free_heap_size();
-      uint32_t min_free_heap = esp_get_minimum_free_heap_size();
+      uint32_t uptime_sec = (uint32_t)(esp_timer_get_time() / 1000000);
       snprintf(json, sizeof(json),
-               "{\"temp\":%.1f,\"hum\":%.0f,\"pres\":%.1f,\"ram\":%lu,\"ramMin\":%lu}",
+               "{\"temp\":%.1f,\"hum\":%.0f,\"pres\":%.1f,\"ram\":%lu,\"up\":%lu}",
                env.temp_aht,
                env.hum_aht,
                env.pres_bmp,
                (unsigned long)free_heap,
-               (unsigned long)min_free_heap);
+               (unsigned long)uptime_sec);
       ws_queue_send(WS_EVENT_TYPE_ENV_UPDATE, json);
     }
     vTaskDelay(pdMS_TO_TICKS(CYCLIC_TASK_ENV_INTERVAL_MS));
