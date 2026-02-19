f9686ecf179216d51808c657d5f48ebfad2b58f8
Miłosz Gilga <gilgamilosz451@gmail.com>
2026-02-19 19:36:08 +0100

    remove previous android code

--- android_control_client/app/src/main/java/pl/miloszgilga/hometheatercontroller/net/ServerConnectivityService.kt
+++
@@ -1,5 +0,0 @@
-package pl.miloszgilga.hometheatercontroller.net
-
-interface ServerConnectivityService {
-  suspend fun isReachable(ip: String, port: Int): Boolean
-}
--- android_control_client/app/src/main/java/pl/miloszgilga/hometheatercontroller/store/AppUiState.kt
+++
@@ -1,8 +0,0 @@
-package pl.miloszgilga.hometheatercontroller.store
-
-data class AppUiState(
-  val ipAddress: String = "192.168.0.2",
-  val restApiPort: String = "8080",
-  val connectionStatus: ConnectionStatus = ConnectionStatus.CONNECTING,
-  val isAppLocked: Boolean = false,
-)
