cd4db1772d91f32666498691868e1ac07d0f42f0
Miłosz Gilga <gilgamilosz451@gmail.com>
2026-02-21 02:58:56 +0100

    feat: add dependencies for camera, networking, and charts

--- android_control_client/app/build.gradle.kts
+++ android_control_client/app/build.gradle.kts
@@ -7,13 +7,13 @@ plugins {
 }
 
 android {
-  namespace = "pl.miloszgilga.hometheatercontroller"
+  namespace = "pl.miloszgilga.htas.client"
   compileSdk {
     version = release(36)
   }
 
   defaultConfig {
-    applicationId = "pl.miloszgilga.hometheatercontroller"
+    applicationId = "pl.miloszgilga.htas"
     minSdk = 26
     targetSdk = 36
     versionCode = 1
@@ -45,22 +45,27 @@ android {
 }
 
 dependencies {
-  implementation(libs.androidx.core.ktx)
-  implementation(libs.androidx.lifecycle.runtime.ktx)
-  implementation(libs.androidx.activity.compose)
-  implementation(libs.androidx.navigation.compose)
-  implementation(platform(libs.androidx.compose.bom))
-  implementation(libs.androidx.compose.ui)
-  implementation(libs.androidx.compose.ui.graphics)
-  implementation(libs.androidx.compose.ui.tooling.preview)
-  implementation(libs.androidx.compose.material3)
-  implementation(libs.androidx.material.icons.extended)
-  implementation(libs.androidx.datastore.preferences)
-  testImplementation(libs.junit)
-  androidTestImplementation(libs.androidx.junit)
-  androidTestImplementation(libs.androidx.espresso.core)
-  androidTestImplementation(platform(libs.androidx.compose.bom))
-  androidTestImplementation(libs.androidx.compose.ui.test.junit4)
-  debugImplementation(libs.androidx.compose.ui.tooling)
-  debugImplementation(libs.androidx.compose.ui.test.manifest)
+  implementation(libs.core.ktx)
+  implementation(libs.lifecycle.runtime.ktx)
+  implementation(libs.activity.compose)
+  implementation(libs.datastore.preferences)
+
+  implementation(platform(libs.compose.bom))
+  implementation(libs.compose.ui)
+  implementation(libs.compose.ui.graphics)
+  implementation(libs.compose.ui.tooling.preview)
+  implementation(libs.compose.material3)
+  implementation(libs.navigation.compose)
+  implementation(libs.material.icons.extended)
+
+  implementation(libs.camera.core)
+  implementation(libs.camera.camera2)
+  implementation(libs.camera.lifecycle)
+  implementation(libs.camera.view)
+  implementation(libs.barcode.scanning)
+  implementation(libs.accompanist.permissions)
+
+  implementation(libs.okhttp)
+  implementation(libs.gson)
+  implementation(libs.mp.android.chart)
 }
